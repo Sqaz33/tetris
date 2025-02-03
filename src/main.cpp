565fbeb0b437e3a599f4dea5bcd552f4afcc0f1f
Matveev Stepan Andreevich <mset321@gmail.com>
2025-02-03 19:08:00 +0300

    feat: add normal keyboard event handler

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <memory>
 #include <optional>
 #include <mutex>
-
+#include <atomic>
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
@@ -16,29 +16,7 @@ using namespace tetris;
 using namespace tetrominoes;
 
 std::mutex mut;
-
-
-void dummyKeyboardHendler(std::shared_ptr<TetrisGameModel> model, sf::RenderWindow& window) {
-    using namespace sf::Keyboard;
-    using namespace std::chrono_literals;
-    // while (window.isOpen()) {
-        {
-            // std::lock_guard<std::mutex> lk{mut};
-            if (isKeyPressed(Key::Left)) {
-                model->moveLeftCurTetromino();
-            } else if (isKeyPressed(Key::Right)) {
-                model->moveRightCurTetromino();
-            } else if (isKeyPressed(Key::Up)) {
-                model->rotateRightCurTetromino();
-            } else if (isKeyPressed(Key::Down)) { 
-                model->update();
-                model->update();
-                model->update();
-            }
-        }
-        // std::this_thread::sleep_for(70ms);
-    // }
-}
+std::atomic<bool> isGameRun = true;
 
 int main() {
     using namespace std::chrono_literals;
@@ -50,21 +28,11 @@ int main() {
 
     comp.pushComponent(drawableField);
 
-    auto closeHandle = [&window] {
-        while (const std::optional event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
-                window.close();
-            }   
-    };
-
-
-    // std::thread t1(dummyKeyboardHendler, model, std::ref(window));
-
     auto modelUpdater = [&] {
         while (window.isOpen()) {
             {
                 std::lock_guard<std::mutex> lk{mut};
-                model->update();
+                isGameRun =  model->update();
             }
             std::this_thread::sleep_for(std::chrono::milliseconds(200ms));
         }
@@ -72,12 +40,44 @@ int main() {
 
     std::thread t2(modelUpdater);
 
+    auto handleEvents = [&] {
+        using namespace sf::Keyboard;
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>() || !isGameRun) {
+                window.close(); 
+                exit(0);
+            }
+            
+            if (!event->is<sf::Event::KeyPressed>()) continue;
+
+            auto keyCode = event->getIf<sf::Event::KeyPressed>()->code;
+
+            switch (keyCode) {
+                case Key::Left:
+                    model->moveLeftCurTetromino();
+                    break;
+                case Key::Right:
+                    model->moveRightCurTetromino();
+                    break;
+                case Key::Up:
+                    model->rotateRightCurTetromino();
+                    break;
+                case Key::Down:
+                    model->update();
+                    model->update();
+                    model->update();
+                    break;
+            }
+
+        }   
+    };
 
     while (window.isOpen()) { 
         {   
             std::lock_guard<std::mutex> lk{mut};
-            closeHandle();
-            dummyKeyboardHendler(model, window);
+            
+            handleEvents();
+
             window.clear();
             comp.draw(window, {0.f, 0.f});
             window.display();
